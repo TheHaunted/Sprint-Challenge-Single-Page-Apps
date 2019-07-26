6829866de08cbf18445946fe675ced009c4d7607
TheHaunted <kevin_lopez@live.com>
2019-07-26 11:52:24 -0500

    completed part 1

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,16 @@
 import React from 'react'
+import {Card} from 'semantic-ui-react';
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
+export default function CharacterCard (props) {
+  return (
+    <div>
+      <Card>
+        <img src={props.image} />
+        <h3>{props.name}</h3>
+        <Card.Description>
+          <p>Location: {props.location.name}</p>
+        </Card.Description>
+      </Card>
+    </div>
+  )
 }
